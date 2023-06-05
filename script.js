37e0e58dfce503eefd7b11ccc54a0bd504685226
Abhishek Sharma <82524461+Abhi7410@users.noreply.github.com>
2023-06-05 22:08:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,81 +1,83 @@
 var images = [
     {
+        id : 1,
         path: "Images/img1.jpeg",
         description:"Cambridge Central Mosque, Mill Road, Cambridge, UK"
     },
     {
+        id : 2,
         path: "Images/img2.jpeg",
         description:"Image of a blue butterfly sitting on a cat's nose"
     },
     {
+        id : 3,
         path: "Images/img3.jpeg",
         description:"Photo of the Taj Mahal taken during sunrise",
     },
     {
+        id : 4,
         path: "Images/img4.jpeg",
         description:"Waterfall in Iceland"
     },
     {
+        id : 5,
         path: "Images/img5.jpeg",
         description:"Female belted kingfisher pictured in Ontario, Canada",
     },
 ]
 
-var img_list = document.getElementById("img-list")
-var current_image = document.getElementById("curr-img")
-var current_desc = document.getElementById("desc_img")
-console.log(img_list)
-changeImage(images[0].path,images[0].description)
+var imageList = document.querySelector(".image-list__list")
+var currentImage = document.querySelector(".viewer-image__img")
+var currentDescription = document.querySelector(".desc-img")
+let currentImageIndex=0;
+changeImage(images[currentImageIndex].path,images[currentImageIndex].description)
 
-
-images.forEach(function(item,index,array){
-    var liItem = document.createElement("li");
+// load the list 
+images.forEach(function(item,index){
+    var listItem = document.createElement("li");
+    listItem.classList.add("image-list__item");
     var img = document.createElement("img");
     img.src = item.path;
     img.alt = `Image ${index+1}`
-    console.log(img)
-    
-    
-    var desc = document.createElement("span")
-    // desc.type = "text"
-    desc.textContent = item.description
-    
-    liItem.appendChild(img)
-    liItem.appendChild(desc)
-    liItem.onclick = function(){
-        changeImage(item.path,item.description)
-        var lstItems = img_list.getElementsByTagName("li");
-        for(let i = 0;i<lstItems.length;i++)lstItems[i].style.backgroundColor="";
-        liItem.style.backgroundColor="lightblue";
-    };
+     
+    var description = document.createElement("span")
+    description.textContent = item.description
 
-    console.log(img)
-    img_list.appendChild(liItem)
+    listItem.appendChild(img)
+    listItem.appendChild(description)
+    listItem.onclick = function(){
+        currentImageIndex = index;
+        changeImage(item.path,item.description)
+        updateSelectedListItem();
+    };
+    imageList.appendChild(listItem)
 })
 
+function updateSelectedListItem() {
+    var lstItems = imageList.getElementsByClassName("image-list__item");
+    for (var i = 0; i < lstItems.length; i++) {
+      if (i === currentImageIndex) {
+        lstItems[i].classList.add("image-list__item--selected");
+        lstItems[i].querySelector("span").textContent = currentDescription.textContent; // Update the description in the list item
+      } else {
+        lstItems[i].classList.remove("image-list__item--selected");
+      }
+    }
+  }
+
+  
 function changeImage(imagePath,imageDesc){
-    current_image.src = imagePath;
-    current_desc.textContent = imageDesc;
+    currentImage.src = imagePath;
+    currentDescription.textContent = imageDesc;
 
 }
 
-current_desc.addEventListener("input",function(){
-   var selectedListItem = img_list.querySelector("li[style='background-color: lightblue;']");
-//    console.log(current_desc.textContent)
-//    console.log(selectedListItem)
-   if (selectedListItem){
-    var spanElement = selectedListItem.querySelector("span")
-    console.log(spanElement.textContent)
-    var old_descript = spanElement.textContent;
-    spanElement.textContent = current_desc.textContent
-
-    var selectedImage = images.find(function(img){
-        return img.description === old_descript ;
-    })
-    if(selectedImage){
-        selectedImage.description = current_desc.textContent;
+currentDescription.addEventListener("input",function(){
+    var selectedImage = images[currentImageIndex];
+    if (selectedImage) {
+        selectedImage.description = currentDescription.textContent;
+        updateSelectedListItem();
     }
-   }
     
 })
 
