918bd2fba924a07d71776146a4b6436a54f5e753
Abhishek Sharma <82524461+Abhi7410@users.noreply.github.com>
2023-06-06 13:34:03 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,23 +4,28 @@ var Model = {
     images : [
         {
           path: "Images/img1.jpeg",
-          description: "Cambridge Central Mosque, Mill Road, Cambridge, UK"
+          description: "Cambridge Central Mosque, Mill Road, Cambridge, UK",
+          count: 0,
         },
         {
           path: "Images/img2.jpeg",
-          description: "Image of a blue butterfly sitting on a cat's nose"
+          description: "Image of a blue butterfly sitting on a cat's nose",
+          count: 0,
         },
         {
           path: "Images/img3.jpeg",
-          description: "Photo of the Taj Mahal taken during sunrise"
+          description: "Photo of the Taj Mahal taken during sunrise",
+          count: 0,
         },
         {
           path: "Images/img4.jpeg",
-          description: "Waterfall in Iceland"
+          description: "Waterfall in Iceland",
+          count: 0,
         },
         {
           path: "Images/img5.jpeg",
-          description: "Female belted kingfisher pictured in Ontario, Canada"
+          description: "Female belted kingfisher pictured in Ontario, Canada",
+          count: 0,
         }
       ],
 
@@ -32,22 +37,23 @@ var Model = {
 // view 
 
 var View = (function(){
-    var imageList = document.querySelector(".image-list__list")
-    var currentImage = document.querySelector(".viewer-image__img")
-    var currentDescription = document.querySelector(".desc-img")
-
+    let imageList = document.querySelector(".image-list__list")
+    let currentImage = document.querySelector(".viewer-image__img")
+    let currentDescription = document.querySelector(".desc-img")
+    let currentImageCount = document.querySelector(".viewer-image__count")
+    console.log(currentImageCount.textContent)
 
     function loadImageList(images){
         // imageList.innerHTML = "";
         images.forEach(function(item,index){
-            var listItem = document.createElement("li");
+            let listItem = document.createElement("li");
             listItem.classList.add("image-list__item");
 
-            var img = document.createElement("img");
+            let img = document.createElement("img");
             img.src = item.path;
             img.alt = "Image " + (index + 1);
 
-            var description = document.createElement("span");
+            let description = document.createElement("span");
             description.textContent = item.description;
             listItem.appendChild(img);
             listItem.appendChild(description);
@@ -55,15 +61,18 @@ var View = (function(){
             listItem.addEventListener('click',function(){
                 Controller.changeImage(item.path, item.description);
                 highlightSelectedImage(index);
+                Controller.changeCount(index);
+                currentImageCount.textContent = "Image" + (index+1) + " Count: " + Controller.showCount(index);
             })
             imageList.appendChild(listItem);
         })
 
     }
 
+
     function highlightSelectedImage(index){
-        var lstItems = imageList.getElementsByClassName("image-list__item");
-        for (var i = 0; i < lstItems.length; i++) {
+        let lstItems = imageList.getElementsByClassName("image-list__item");
+        for (let i = 0; i < lstItems.length; i++) {
             if (i === index) {
               lstItems[i].classList.add("image-list__item--selected");
               lstItems[i].querySelector("span").textContent = currentDescription.textContent; // Update the description in the list item
@@ -87,7 +96,7 @@ var View = (function(){
     }
 
     function updateSelectedListItem() {
-        var selectedItem = imageList.querySelector(".image-list__item--selected");
+        let selectedItem = imageList.querySelector(".image-list__item--selected");
         if (selectedItem) {
           selectedItem.querySelector("span").textContent = currentDescription.textContent;
         }
@@ -95,7 +104,7 @@ var View = (function(){
 
 
     currentDescription.addEventListener("input", function() {
-        var indexOfImage = Array.from(imageList.children).indexOf(imageList.querySelector(".image-list__item--selected"));
+        let indexOfImage = Array.from(imageList.children).indexOf(imageList.querySelector(".image-list__item--selected"));
         console.log(indexOfImage)
         Controller.updateImageDescription(indexOfImage);
         updateSelectedListItem();
@@ -119,6 +128,14 @@ var Controller = (function() {
     View.loadImageList(Model.images);
     changeImage(Model.images[0].path,Model.images[0].description)
     }
+
+    function changeCount(index){
+        Model.images[index].count++;
+    }
+
+    function showCount(index){
+        return Model.images[index].count;
+    }
   
     function changeImage(imagePath, imageDesc) {
       View.setCurrentImage(imagePath);
@@ -126,8 +143,8 @@ var Controller = (function() {
     }
 
     function updateImageDescription(index) {
-        var selectedImage = Model.getSelectedImage(index);
-        var newDescription = View.getCurrentDescription();
+        let selectedImage = Model.getSelectedImage(index);
+        let newDescription = View.getCurrentDescription();
         selectedImage.description = newDescription;
     }
   
@@ -135,6 +152,8 @@ var Controller = (function() {
       init: init,
       changeImage: changeImage,
       updateImageDescription: updateImageDescription,
+      changeCount: changeCount,
+      showCount: showCount,
     };
   })();
   
